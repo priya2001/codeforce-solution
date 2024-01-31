17429e7e2c9fff008c27bdec7e7a445144a44b13
Priya Gautam <priyagautam1401@gmail.com>
2024-01-31 14:14:55 +0530

    Brick Wall

---
+++ A922.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int A=n*(x/2);
+        cout<<A<<endl;
+    }
+}
+
+
+
+
+
