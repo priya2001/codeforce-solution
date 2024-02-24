a34790457c41fbf5f4660f6c46c7ab922b8f9b93
Priya Gautam <priyagautam1401@gmail.com>
2024-02-25 00:17:22 +0530

    odd queries

---
+++ C859.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int t, n;
+string a;
+int f = 1;
+int main()
+{
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> a;
+        for (int i = 0; i < n; i += 2)
+        {
+            for (int j = 1; j < n; j += 2)
+            {
+                if (a[i] == a[j])
+                {
+                    f = 0;
+                }
+            }
+        }
+        if (f == 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+        f = 1;
+    }
+    return 0;
+}
